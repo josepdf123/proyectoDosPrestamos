726fd3fc591743a79740e6ea1e2074edf2c2bd04
Elwhit <105029343+Elwhit@users.noreply.github.com>
2026-03-11 18:03:13 -0500

    Cambio a render

--- config/settings.py
+++ config/settings.py
@@ -51,11 +51,14 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
+import dj_database_url
+
+# PostgreSQL en Render
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='sqlite:///db.sqlite3',
+        conn_max_age=600
+    )
 }
 
 AUTH_USER_MODEL = 'accounts.Usuario'
